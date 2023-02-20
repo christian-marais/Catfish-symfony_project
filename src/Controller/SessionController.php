58b504b9d53bdfb4ec921cfc22915ff0d94ebf0b
christian marais <christimarais@hotmail.fr>
2023-02-20 23:47:41 +0400

    correction du controller de session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -20,7 +20,6 @@ class SessionController extends AbstractController
         }else{
             $nbreVisite = 1;
         }
-        if()
         $session->set('nbreVisite',$nbreVisite);
         return $this->render('session/index.html.twig', [
             'controller_name' => 'SessionController',
