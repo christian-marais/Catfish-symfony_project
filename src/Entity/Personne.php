a2c12994abe45c57585a2748f40dd93a86f2f56d
christian marais <christimarais@hotmail.fr>
2023-02-21 23:48:20 +0400

    ajout de la propriété/champ job dans personne et synchro

--- src/Entity/Personne.php
+++ src/Entity/Personne.php
@@ -22,6 +22,9 @@ class Personne
     #[ORM\Column(nullable: true)]
     private ?int $age = null;
 
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $job = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -59,6 +62,18 @@ class Personne
     public function setAge(?int $age): self
     {
         $this->age = $age;
+
+        return $this;
+    }
+
+    public function getJob(): ?string
+    {
+        return $this->job;
+    }
+
+    public function setJob(?string $job): self
+    {
+        $this->job = $job;
 
         return $this;
     }
