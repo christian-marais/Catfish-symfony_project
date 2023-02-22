e83d5250a193edf0ed39aa1048d8dee96ac812e8
christian marais <christimarais@hotmail.fr>
2023-02-22 07:34:10 +0400

    installation  des fixtures et du faker pour personne

---
+++ src/DataFixtures/PersonneFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Personne;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class PersonneFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {   
+        $faker= Factory::create(locale:'fr_FR');
+        for($i=0;$i<20;$i++){
+
+            $personne = new Personne();
+            $personne->setFirstname(firstname:$faker->firstName);
+            $personne->setName(name:$faker->lastName);
+            $personne->setAge(age:$faker->numberBetween(18,60));
+
+            $manager->persist($personne);
+
+        }
+      
+
+       
+
+        $manager->flush();
+    }
+}
