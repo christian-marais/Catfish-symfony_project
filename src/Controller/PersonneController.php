1c0eb9c8feb5c4fa0055cf859e9dade59fba2845
christian marais <christimarais@hotmail.fr>
2023-02-22 06:32:55 +0400

    ajout de la route pour ajouter une personne dans la bdd et affichage de l'execution dans twig

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +16,24 @@ class PersonneController extends AbstractController
         return $this->render('personne/index.html.twig', [
             'controller_name' => 'PersonneController',
         ]);
+    }
+    #[Route(path:'/personne/add/', name:'personne.add')]
+    public function addPersonne(ManagerRegistry $doctrine):Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $personne = new Personne();
+        $personne->setFirstname(firstname:'Jean');
+        $personne->setName(name:'Bizance');
+        $personne->setAge(age:22);
+
+        $entityManager->persist($personne);
+        if($entityManager->flush()){
+            $this->addFlash(
+                type:'success',
+                message:'la personne a'.$personne->getName().' été créé');
+        }
+
+        return $this->render(view:'personne/index.html.twig', parameters: array('personne' => $personne));
     }
 }
